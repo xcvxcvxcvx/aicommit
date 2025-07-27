25d22b392739c6dc95bd8f02cf59a0392128633b
Yrey Metalnikov <yuriy.metalnikov@messagemedia.com>
2025-07-27 21:48:43 +0200

    feat(git): implement exit on empty diff
     - Exit the program if git diff is empty.

--- git.go
+++ git.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,9 +10,25 @@ import (
 func GenerateGit() string {
 	cmd := exec.Command("git", "diff", "--cached")
 	out, err := cmd.Output()
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка при выполнении git diff: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error when trying git diff: %v\n", err)
 		os.Exit(1)
 	}
+
+	lines := bytes.Split(out, []byte{'\n'})
+	var hasContent bool
+	for _, line := range lines {
+		trimmed := bytes.TrimSpace(line)
+		if len(trimmed) > 0 && !bytes.HasPrefix(trimmed, []byte("#")) {
+			hasContent = true
+			break
+		}
+	}
+
+	if !hasContent {
+		os.Exit(0)
+	}
+
 	return string(out)
 }
